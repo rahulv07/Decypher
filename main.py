3e1c0c505fcf495f6dc372466bf9c8a4d84b0bab
Rahul <rahulsudhan2601@gmail.com>
2021-11-30 23:24:11 +0530

    Add CLI arguments

--- main.py
+++ main.py
@@ -1,5 +1,13 @@
 import filter
+import argparse
 
-videoName = "rahul.mp4"
+
+parser = argparse.ArgumentParser()
+parser.add_argument("path",help = "Enter the video file path")
+args = parser.parse_args()
+
+videoName = args.path
+
 audioName = filter.extractAudio(videoName)
 filter.preProcess(audioName)
+
